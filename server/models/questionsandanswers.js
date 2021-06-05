ae2669bcfe366d9ff66759304327d6ecef6293c3
Jenn Ngai <jkwngai@gmail.com>
2021-06-05 11:13:40 -0700

    Completed getAllQuestionsAndAnswers and getAllReviews

--- server/models/questionsandanswers.js
+++ server/models/questionsandanswers.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 const config = require('../../config.js');
 
 let getAllQuestionsAnswers = (callback, productId) => {
+  const id = productId.product_id;
   let options = {
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions?product_id=${productId}`,
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions?product_id=${id}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': config.TOKEN
